1f7d9459d65ed04a8795fc90f6fa5ed2464e8997
Peter Berkenbosch <peter@pero-ict.nl>
2012-08-22 15:54:07 +0100

    fix the authorize_net spec

--- app/models/spree/gateway/authorize_net.rb
+++ app/models/spree/gateway/authorize_net.rb
@@ -2,9 +2,10 @@ module Spree
   class Gateway::AuthorizeNet < Gateway
     preference :login, :string
     preference :password, :string
-    
+    preference :test_mode, :boolean
+
     attr_accessible :preferred_login, :preferred_password
-    
+
     def provider_class
       ActiveMerchant::Billing::AuthorizeNetGateway
     end
